9bf539a1d1efa1a58c84fbd130d219abdc1c39e6
kaffan <53649298+kaffan@users.noreply.github.com>
2021-01-31 21:38:59 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+      $(".box1").css("background-color","yellow");
+	  $(".center4").css("background-color","red");
+	  $(".center5").css("background-color","lightblue");
+	  $(".center6").css("background-color","blue");
+	  $("tr:odd").css("background-color","gray");
+	  $(".compimg").dblclick(function(){
+			$(".compimg").hide();
+		});
+	  $(".mechimg").dblclick(function(){
+			$(".mechimg").hide();
+		});
+	  $(".bioimg").dblclick(function(){
+			$(".bioimg").hide();
+		});
+	  $(".extcimg").dblclick(function(){
+			$(".extcimg").hide();
+		});	
+	
+		
+     });
---
+++ my1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+ $('[name="first"]').css("background-color","purple");
+	  $('[name="second"]').css("background-color","purple");
+      $('[name="submit"]').css("background-color","pink");
+	  $('[name="email"]').css("background-color","blue");
+	
+	  
+});
